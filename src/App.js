f440cf00897abffcada2b02f7acf00e96ffdae8e
tareq_abdelhaq <tareq.abdelhaq98@gmail.com>
2023-12-04 02:34:52 +0200

    feat: edit quiz page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import AllQuizesPage from "./pages/AllQuizesPage";
 import CreateQuizPage from "./pages/CreateQuizPage";
+import EditQuizPage from "./pages/EditQuizPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AllQuizesPage />} />
       <Route path="/create-quiz" element={<CreateQuizPage />} />
+      <Route path="/quiz/:quizId/edit" element={<EditQuizPage />} />
       <Route path="*" element={<Navigate to="/" replace={true} />} />
     </Routes>
   );
